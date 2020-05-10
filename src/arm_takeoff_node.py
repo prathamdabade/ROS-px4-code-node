f8cdb68ae411f1f63f4637982ba61c4cf4de2772
prathamdabade <prathamdabade@gmail.com>
2020-05-10 20:09:51 +0530

    Added Working Takeoff code

--- src/arm_takeoff_node.py
+++ src/arm_takeoff_node.py
@@ -1,31 +1,64 @@
-#! /usr/bin/env python
+#!/usr/bin/env python
 
 import rospy
+import mavros
+from geometry_msgs.msg import PoseStamped
 from mavros_msgs.msg import State
-from mavros_msgs.srv import CommandBool
-from mavros_msgs.srv import CommandTOL
-
-def arm():
-  rospy.wait_for_service('/mavros/cmd/arming')
-  try:
-    arm = rospy.ServiceProxy('/mavros/cmd/arming',CommandBool)
-    arm(True)
-  except rospy.ServiceException:
-    print('could not arm')
+from mavros_msgs.srv import CommandBool, SetMode
 
 
-def takeoff():
-  rospy.wait_for_service('/mavros/cmd/takeoff')
-  try:
-    takeoff = rospy.ServiceProxy('/mavros/cmd/takeoff',CommandTOL)
-    takeoff(altitude=10)
-  except rospy.ServiceException:
-    print('could not takeoff')
+current_State = State()
 
+def callback(msg):
+  global current_State
+  current_State = msg
+  
 def main():
-  pass
+  global current_State
 
+  rospy.init_node('offboard', anonymous=True)
+  r = rospy.Rate(20)
+  pos_pub = rospy.Publisher('/mavros/setpoint_position/local', PoseStamped, queue_size=20)
+  rospy.Subscriber('/mavros/state', State, callback)
+  arming = rospy.ServiceProxy('/mavros/cmd/arming', CommandBool)
+  setmode = rospy.ServiceProxy('/mavros/set_mode', SetMode)
 
+  pos = PoseStamped()
+  height = float(input("Enter takeoff Height:"))
+  pos.pose.position.z = height
+  prev_state = current_State
+
+  for i in range(50):
+    pos_pub.publish(pos)
+    r.sleep()
+
+  while not current_State.connected:
+    r.sleep()
+  
+  t0 = rospy.get_rostime()
+
+  while not rospy.is_shutdown():
+    now = rospy.get_rostime()
+
+    if current_State.mode != "OFFBOARD" and (now-t0 > rospy.Duration(5)):
+      setmode(base_mode=0, custom_mode="OFFBOARD")
+      t0=now
+
+    else:
+      if not current_State.armed and(now-t0 > rospy.Duration(5)):
+        arming(True)
+        t0=now
+
+    if prev_state.armed != current_State.armed:
+      rospy.loginfo("Vehicle armed: %r" % current_State.armed)
+    if prev_state.mode != current_State.mode:
+      rospy.loginfo("current mode: %s" % current_State.mode)
+    prev_state = current_State
+
+    pos.header.stamp = rospy.Time.now()
+    pos_pub.publish(pos)
+    
+    r.sleep()
 
 if __name__ == "__main__":
   try:
